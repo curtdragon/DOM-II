43b8975d439f5e72d1450367d78e865f4efede75
curtdragon <tealturboser@gmail.com>
2019-05-03 23:02:28 -0500

    Mouseover and Keydown Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
 // Your code goes here
+
+//mouseover
+let magicbus = document.getElementById("funbus");
+
+magicbus.addEventListener("mouseover", event => {
+    event.target.style.visibility="hidden";
+    setTimeout(function () {
+        event.target.style.visibility = "visible";
+    }, 500);
+}, {once:true}
+);
+
+//keydown
+document.addEventListener("keydown",event => {
+    alert("You are supposed to be reading not typing!!!!");
+    },
+    {once:true}
+);
+
+//wheel
+
+//drag/drop
+
+//load
+let dest_img = document.querySelector("#destination_img");
+
+dest_img.addEventListener('load', () => {
+    alert("Loaded");
+});
+
+//focus
+let focusbutton = document.
+getElementById("destination1");
+console.log(focusbutton);
+focusbutton.addEventListener('focus',event => {
+    event.target.style.fontcolor = "orange";
+    alert("Button Focus");
+});
+//resize
+
+//scroll
+
+//select
+let textselect = document.getElementById("destinationh2");
+console.log(textselect);
+textselect.addEventListener('select', event => {
+    alert("hello");
+});
+//dblclick
+
+//nested
+
+//prevent refresh
+
+//Strech task: GASP
+
